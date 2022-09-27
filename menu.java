cbcf30293d52d6556cc2fc81d534fce0867b9f6f
Catt McHale <91563580+thealyx0ne@users.noreply.github.com>
2022-09-27 12:25:59 -0400

    making required files

---
+++ menu.java
@@ -0,0 +1,8 @@
+FileSystem.out.println("Select a game: ");
+FileSystem.out.println("1: Tic Tac Toe");
+FileSystem.out.println("2: Frogger");
+FileSystem.out.println("3: Pong");
+FileSystem.out.println("Z: Quit");
+
+
+

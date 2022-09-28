84743093b14ccc6e6241488a2974cce1ec990258
Catt McHale <91563580+thealyx0ne@users.noreply.github.com>
2022-09-28 11:33:56 -0400

    Update menu.java

--- menu.java
+++ menu.java
@@ -1,8 +1,13 @@
-FileSystem.out.println("Select a game: ");
-FileSystem.out.println("1: Tic Tac Toe");
-FileSystem.out.println("2: Frogger");
-FileSystem.out.println("3: Pong");
-FileSystem.out.println("Z: Quit");
+public class Main{
+    public static void main(String[] args){ 
+        String[] menu = {"0: Frogger", "1: Pong", "2: Tic Tac Toe", "3: Quit"}; //menu array for user selection
+        System.out.println(menu[0]);
+        System.out.println(menu[1]);
+        System.out.println(menu[2]);
+        System.out.println(menu[3]);//prints menu
+    }
+}
+    
 
 
 # based on menu select, menu will refence correct file and run that code

2584b207bbad0fccd6409ffe72fd3de5b23ea3a6
Catt McHale <91563580+thealyx0ne@users.noreply.github.com>
2022-10-12 11:28:14 -0400

    add imports, add shell for game

--- pong.java
+++ pong.java
@@ -1,5 +1,50 @@
-#open gui from menu select
+//open gui from menu select
+import java.util.Scanner;
+import java.fx;
 
-public class Pong{
-    
+
+public class Pong {
+    public boolean 
+    public boolean alive = false;
+    public int points = 0;
+
+
+
+    public void paddle1() {
+        //set position
+        //get position
+        //get keyboard input
+        ///on left arrow hold, move left @ ?? speed
+        ///on right arrow hold, move right @ ?? speed
+        //get ball position
+
+
+    }
+    public void paddle2() {
+        //set position
+        //get position
+        //set move rate
+        //get ball position
+
+    }
+    public void ball() {
+        //getter
+        //setter
+        //setPosition ball() = (500,500)
+        //if ballPosition == hit wall 
+        //player lose
+        //else
+        //return;
+
+    }
+    pubic void points() {
+        //track player points
+        //one point per win
+        //ball speed increase +5 each round
+        //player 2 speed increase +5 each round
+        //while alive = true
+        /// ...
+        //if alive = false
+        //return to main menu
+    }
 }

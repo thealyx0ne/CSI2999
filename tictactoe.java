9b4a7a988af998959173b11c150070e49aa5dae1
TheKossMan <114510465+TheKossMan@users.noreply.github.com>
2022-10-26 01:34:45 -0400

    Update tictactoe.java
    Startup code, and the start of the while loop system

--- tictactoe.java
+++ tictactoe.java
@@ -1,14 +1,62 @@
-//open gui from menu select
-import java.util.Scanner;
-//import javafx;
+package tictactoe;
+
+/**
+ *
+ * @author Ian Koss
+ */
+import java.util.*;
+import java.lang.Math;   
 
 public class TicTacToe {
-    public String x = "x";
-    public String o = "o";
-    public int turn = 0;
-
+    public static String x = "x";
+    public static String o = "o";
+    public static int turn = 0; 
+    public static boolean winner;
+    public static int coin = 0;
+    
     public static void main(String[] args) {
+        startup();   
+        while(winner == false){
+            
+        }
+    }
+    
+    public static void startup(){
+        Random random = new Random();
+        Scanner scan = new Scanner(System.in); 
         
+        System.out.println("Enter name of Player 1: ");
+        String playerOne = scan.nextLine(); 
+        System.out.println("");
+        
+        System.out.println("Enter name of Player 2: ");
+        String playerTwo = scan.nextLine(); 
+        System.out.println("");
+        
+        System.out.println(playerOne + ", choose 1 or 2: ");
+        int playerOneChoice = scan.nextInt(); 
+        System.out.println("");
+        
+        System.out.println(playerOne + " chose " + playerOneChoice + ", which "
+                + "means " + playerTwo + " chooses the other.");
+        
+        coin = random.nextInt(2-1) + 1;
+        String firstTurnPlayer;
+        String secondTurnPlayer;
+        
+        if(playerOneChoice == 1){
+            firstTurnPlayer = playerOne;
+            secondTurnPlayer = playerTwo;
+        }else{
+            firstTurnPlayer = playerTwo;
+            secondTurnPlayer = playerOne;
+        }
+        
+        System.out.println("The random number was " + coin + ", which means " + 
+                firstTurnPlayer + " goes first.");    
+        System.out.println("");
+        System.out.println(firstTurnPlayer + " are the o's, and " + playerTwo +
+                " are the x's.");
     }
     
     public void turn(){
@@ -16,6 +64,7 @@ public class TicTacToe {
     }
     
     public boolean winner(){
+        System.out.println("The winner is: ");
         return true;
     }
     
@@ -28,6 +77,7 @@ public class TicTacToe {
     }
     
     public String board(){
-        return "board";
+        String board = "Now";
+        return board;
     }
 }

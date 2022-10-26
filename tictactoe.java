f2ab1b6eaf9c6bec8860cb251c3d436a259d94b6
TheKossMan <114510465+TheKossMan@users.noreply.github.com>
2022-10-26 10:57:07 -0400

    Update tictactoe.java

--- tictactoe.java
+++ tictactoe.java
@@ -8,13 +8,15 @@ import java.util.*;
 import java.lang.Math;   
 
 public class TicTacToe {
-    public static String x = "x";
-    public static String o = "o";
+    public static String xs = "x";
+    public static String os = "o";
     public static int turn = 0; 
     public static boolean winner;
     public static int coin = 0;
+    static String[] board;
     
     public static void main(String[] args) {
+        board = new String[9];
         startup();   
         while(winner == false){
             
@@ -25,6 +27,7 @@ public class TicTacToe {
         Random random = new Random();
         Scanner scan = new Scanner(System.in); 
         
+        System.out.println("Welcome to Tic-Tac-Toe!");
         System.out.println("Enter name of Player 1: ");
         String playerOne = scan.nextLine(); 
         System.out.println("");
@@ -57,27 +60,47 @@ public class TicTacToe {
         System.out.println("");
         System.out.println(firstTurnPlayer + " are the o's, and " + playerTwo +
                 " are the x's.");
+        
+        for (int a = 0; a < 9; a++) {
+            board[a] = String.valueOf(a + 1);
+        }
+        
+        pasteBoard();
+        System.out.println("");
+        System.out.println("When playing and on your turn, put the number in"
+                + "for the spot you want!");
     }
     
-    public void turn(){
+    static void turn(){
         
     }
     
-    public boolean winner(){
+    static boolean winner(){
         System.out.println("The winner is: ");
         return true;
     }
     
-    public void player1(){
+    static void player1(){
         
     }
     
-    public void player2(){
+    static void player2(){
         
     }
     
-    public String board(){
-        String board = "Now";
-        return board;
+    static void pasteBoard(){
+        System.out.println("|---|---|---|");
+        System.out.println("| " + board[0] + " | "
+                           + board[1] + " | " + board[2]
+                           + " |");
+        System.out.println("|-----------|");
+        System.out.println("| " + board[3] + " | "
+                           + board[4] + " | " + board[5]
+                           + " |");
+        System.out.println("|-----------|");
+        System.out.println("| " + board[6] + " | "
+                           + board[7] + " | " + board[8]
+                           + " |");
+        System.out.println("|---|---|---|");
     }
 }

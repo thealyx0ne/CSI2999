70e2691678238a4fdacd103c21b5f97cf3ba340d
TheKossMan <114510465+TheKossMan@users.noreply.github.com>
2022-11-22 16:31:50 -0500

    Update tictactoe.java
    Game is completed

--- tictactoe.java
+++ tictactoe.java
@@ -185,6 +185,58 @@ public class TicTacToe {
     }
     
     static boolean checkWinner(){
+        for (int a = 0; a < 8; a++) {
+            String line = null;
+ 
+            switch (a) {
+            case 0:
+                line = board[0] + board[1] + board[2];
+                break;
+            case 1:
+                line = board[3] + board[4] + board[5];
+                break;
+            case 2:
+                line = board[6] + board[7] + board[8];
+                break;
+            case 3:
+                line = board[0] + board[3] + board[6];
+                break;
+            case 4:
+                line = board[1] + board[4] + board[7];
+                break;
+            case 5:
+                line = board[2] + board[5] + board[8];
+                break;
+            case 6:
+                line = board[0] + board[4] + board[8];
+                break;
+            case 7:
+                line = board[2] + board[4] + board[6];
+                break;
+            }
+            //For X winner
+            if (line.equals("xxx")) {
+                winner = firstTurnPlayer;
+                return true;
+            }
+             
+            // For O winner
+            else if (line.equals("ooo")) {
+                winner = secondTurnPlayer;
+                return true;
+            }   
+        }
+        for (int a = 0; a < 9; a++) {
+            if (Arrays.asList(board).contains(
+                    String.valueOf(a + 1))) {
+                break;
+            }
+            else if (a == 8) {
+                System.out.println("No one wins... it is a draw!");
+                System.exit(0);
+                return false;
+            }
+        }
         return false;
     }
 }
